77c72693aec5cde2e8a550df6f3239c88f719530
Kristine Johnson <100108249+kristinejohnson7@users.noreply.github.com>
2023-01-05 13:05:08 -0500

    update multer uploads path

--- routes/posts.js
+++ routes/posts.js
@@ -5,9 +5,10 @@ const Post = require("../models/Post");
 const multer = require("multer");
 
 const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, "./uploads");
-  },
+  destination: require.main?.path + "/" + " public/uploads/",
+  // function (req, file, cb) {
+  //   cb(null, "public/images/");
+  // }
   filename: function (req, file, cb) {
     cb(null, Date.now() + "_" + file.originalname);
   },
